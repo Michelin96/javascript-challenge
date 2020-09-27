a691ccc13fcd5274daf31990e226a69e298ab4eb
MichelleH <voirdire96@gmail.com>
2020-09-26 19:45:46 -0700

    Loaded table into html

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,4 +1,53 @@
 // from data.js
-var tableData = data;
+let tableData = data;
 
-// YOUR CODE HERE!
+// Use D3 to select the table body
+let tbody = d3.select("tbody");
+
+// Use D3 to select the table
+let table = d3.select("table");
+
+let filterButton = d3.select("#filter-btn"),
+    form = d3.select(".form-group");
+
+// Make a table of all the sighting data
+tableData.forEach(sighting => {
+    let row = tbody.append("tr");
+    row.append("td").text(sighting.datetime);
+    console.log(sighting.datetime);
+
+    row.append("td").text(sighting.city);
+    row.append("td").text(sighting.state);
+    row.append("td").text(sighting.country);
+    row.append("td").text(sighting.shape);
+    row.append("td").text(sighting.durationMinutes);
+    row.append("td").text(sighting.comments);
+})
+
+// Data array object for reference
+//     datetime: "1/1/2010",
+//     city: "benton",
+//     state: "ar",
+//     country: "us",
+//     shape: "circle",
+//     durationMinutes: "5 mins.",
+//     comments: "4 bright green circles high in the sky going in circles then one bright green light at my front door."
+
+// Complete the event handler function for the form
+function runEnter() {
+    // Prevent the page from refreshing
+    d3.event.preventDefault();
+    // Select the input element and get the raw HTML node
+    let inputEntry = d3.select("#datetime");
+  
+    // Get the value property of the input element
+    let inputValue = inputEntry.property("value");
+  
+    console.log(inputValue);
+
+};
+
+
+// Create event handlers 
+button.on("click", runEnter);
+form.on("submit",runEnter);
